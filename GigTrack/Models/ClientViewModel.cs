bf21260bf04de8b9a2bea6492b3ecad72bd98904
Joel Gage <jrossgage@gmail.com>
2021-07-26 14:37:24 -0500

    Gig list and Client list back end is returning the appropriate data. Along with a View model for clients that allows for gig pay to be assoiciated to a specific client in a list

--- GigTrack/Models/ClientViewModel.cs
+++ GigTrack/Models/ClientViewModel.cs
@@ -5,15 +5,24 @@ using System.Threading.Tasks;
 
 namespace GigTrack.Models
 {
-    public class Client
+    public class ClientViewModel
     {
         public int Id { get; set; }
         public string CompanyName { get; set; }
         public int PhoneNumber { get; set; }
         public string Email { get; set; }
         public int UserId { get; set; }
+        public List<int> GigPay { get; set; }
 
-      
-
+        public int PaySum
+        { get {
+                int total = 0;
+                foreach(int i in GigPay)
+                {
+                    total += i;
+                }
+                return total;
+               }
+        }
     }
 }
---
+++ GigTrack/Repositories/ClientRepository.cs
@@ -0,0 +1,85 @@
+﻿using GigTrack.Models;
+using GigTrack.Utils;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GigTrack.Repositories
+{
+    public class ClientRepository : BaseRepository, IClientRepository
+    {
+        public ClientRepository(IConfiguration config) : base(config) { }
+
+        public List<ClientViewModel> GetAllClientsByFirebaseId(string firebaseUserId)
+        {
+            //TODO: I need a list of ints of the gig pay associated with each client to be able to do some logic to
+            //determine if a client has paid the user $600 or more.
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                              SELECT 
+                              c.id AS ClientId, c.companyName, c.phoneNumber, c.email, c.userId,
+                              g.id, g.pay, 
+                              u.id, u.[name], u.[email], u.firebaseUserId
+                         FROM Client c
+                              LEFT JOIN Gig g ON g.clientId = c.id
+                              LEFT JOIN UserProfile u ON u.id = c.userId
+                        WHERE firebaseUserId = @firebaseUserId
+                         ORDER BY c.companyName DESC";
+                    DbUtils.AddParameter(cmd, "@firebaseUserId", firebaseUserId);
+                    var reader = cmd.ExecuteReader();
+
+                    var clients = new List<ClientViewModel>();
+
+                    while (reader.Read())
+                    {
+                        var clientId = DbUtils.GetInt(reader, "ClientId");
+                        var existingClient = clients.FirstOrDefault(c => c.Id == clientId);
+                        if (existingClient == null)
+                        {
+                            existingClient = NewClientVMFromReader(reader);
+                            clients.Add(existingClient);
+                        };
+                        existingClient.GigPay.Add(DbUtils.GetInt(reader, "pay"));
+
+                    }
+
+                    reader.Close();
+
+                    return clients;
+                }
+            }
+        }
+
+        private ClientViewModel NewClientVMFromReader(SqlDataReader reader)
+        {
+            return new ClientViewModel()
+            {
+                Id = DbUtils.GetInt(reader, "id"),
+                CompanyName = DbUtils.GetString(reader, "companyName"),
+                PhoneNumber = DbUtils.GetInt(reader, "phoneNumber"),
+                Email = DbUtils.GetString(reader, "email"),
+                UserId = DbUtils.GetInt(reader, "userId"),
+                GigPay = new List<int>()
+            };
+        }
+        private Client NewClientFromReader(SqlDataReader reader)
+        {
+            return new Client()
+            {
+                Id = DbUtils.GetInt(reader, "id"),
+                CompanyName = DbUtils.GetString(reader, "companyName"),
+                PhoneNumber = DbUtils.GetInt(reader, "phoneNumber"),
+                Email = DbUtils.GetString(reader, "email"),
+                UserId = DbUtils.GetInt(reader, "userId"),
+
+            };
+        }
+    }
+}
