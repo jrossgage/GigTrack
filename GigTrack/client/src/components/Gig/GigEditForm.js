2a839070103425e88257f3f3b1b8aad15c8d5b4a
Joel Gage <jrossgage@gmail.com>
2021-07-28 11:00:09 -0500

    Gig edit form is working

---
+++ GigTrack/client/src/components/Gig/GigEditForm.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { getAllLocations } from '../../modules/locationManager';
+import { getAllClients } from '../../modules/clientManager';
+import { getGigById, updateGig } from '../../modules/gigManager';
+
+const GigEditForm = () => {
+    const [editGig, setEditGig] = useState({});
+    const [locations, setLocations] = useState([]);
+    const [clients, setClients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { id } = useParams();
+    const history = useHistory();
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        const key = e.target.id;
+
+        const gigCopy = { ...editGig };
+
+        gigCopy[key] = value;
+        setEditGig(gigCopy);
+    };
+
+    const getLocations = () => {
+        return getAllLocations()
+            .then(locationFromAPI => {
+                setLocations(locationFromAPI)
+            })
+    }
+
+    const getClients = () => {
+        return getAllClients()
+            .then(clientsFromAPI => {
+                setClients(clientsFromAPI)
+            })
+    }
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        const editedGig = {
+            id: id,
+            pay: editGig.pay,
+            date: editGig.date,
+            mileage: editGig.mileage,
+            clientId: editGig.clientId,
+            venueName: editGig.venueName,
+            locationId: editGig.locationId,
+            notes: editGig.notes,
+        };
+        updateGig(editedGig)
+            .then((g) => {
+                history.push(`/gig/details/${editedGig.id}`);
+            });
+    };
+
+    useEffect(() => {
+        getClients();
+        getLocations();
+        getGigById(id)
+            .then(g => {
+                setEditGig(g);
+                setIsLoading(false)
+            });
+    }, [id])
+
+    return (
+        <Form className="container w-75">
+            <h2>Edit Gig</h2>
+            <FormGroup>
+                <Label for="Venue">Venue</Label>
+                <Input type="text" name="venueName" id="venueName" placeholder="Venue"
+                    value={editGig.venueName}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="pay">Pay</Label>
+                <Input type="number" name="pay" id="pay" placeholder="pay"
+                    value={editGig.pay}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="mileage">Mileage</Label>
+                <Input type="number" name="mileage" id="mileage" placeholder="mileage"
+                    value={editGig.mileage}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="date">Date</Label>
+                <Input type="date" name="date" id="date" placeholder="date"
+                    value={editGig.date}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="Client">Client </Label>
+                <select value={editGig.clientId} name="clientId" id="clientId" onChange={handleInputChange} className='form-control'>
+                    <option value="0">Select Client</option>
+                    {clients.map(c => (
+                        <option key={c.id} value={c.id}>{c.companyName}</option>
+                    ))}
+                </select>
+            </FormGroup>
+            <FormGroup>
+                <Label for="Location">Location</Label>
+                <select value={editGig.locationId} name="locationId" id="locationId" onChange={handleInputChange} className='form-control'>
+                    <option value="0">Select Location</option>
+                    {locations.map(l => (
+                        <option key={l.id} value={l.id}>{`${l.city}, ${l.state}`}</option>
+                    ))}
+                </select>
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="notes">Notes</Label>
+
+                <textarea type="text" name="notes" id="notes" placeholder="notes"
+                    value={editGig.notes}
+                    onChange={handleInputChange} rows="10" cols="145" />
+
+            </FormGroup>
+
+            <Button className="btn btn-primary" onClick={handleUpdate}>Save</Button>
+            <Button className="btn btn-primary" onClick={() => history.push(`/gig`)}>Cancel</Button>
+
+        </Form>
+    )
+}
+export default GigEditForm;
